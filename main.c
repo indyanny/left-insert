6307074eb49ccd826069627b578d78b3df3d7a0e
indyanny <indy.engenharia@gmail.com>
2024-03-30 20:37:25 -0300

    left-insert

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    struct no{
+        int dado;
+        struct no *prox;
+    };
+
+    struct no *lista, *p;
+    int sn;
+
+    lista = NULL;
+
+    do{
+        p = malloc(sizeof(struct no));
+        printf("Digite um dado a inserir na lista:");
+        scanf("%i", &p->prox);
+        p->prox = lista;
+        lista = p;
+
+        printf("Deseja inserir outro no a esquerda? 0-nao 1-sim");
+        scanf("%i", &sn);
+    }
+    while(sn == 1);
+
+    printf("\n\n");
+    p = lista;
+    while(p != NULL){
+        printf("%i", p->dado);
+        p = p->prox;
+    }
+
+    return 0;
+}
